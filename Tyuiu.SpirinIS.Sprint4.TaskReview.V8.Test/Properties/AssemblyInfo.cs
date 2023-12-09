38c6bc16691c140d77b705cf655cfc5d8bb51761
Tyuiu.MartysevichAD <Tyuiu.MartysevichAD@outlook.com>
2023-12-09 12:54:33 +0500

    Add project files.

---
+++ Tyuiu.SpirinIS.Sprint4.TaskReview.V8.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SpirinIS.Sprint4.TaskReview.V8.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SpirinIS.Sprint4.TaskReview.V8.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d129a8c-40ac-4997-935c-2ed788a8de5a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SpirinIS.Sprint4.TaskReview.V8/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SpirinIS.Sprint4.TaskReview.V8.Lib;
+
+namespace Tyuiu.SpirinIS.Sprint4.TaskReview.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Мартысевич А. Д. | ИИПб-23-3";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #4                                                              *");
+            Console.WriteLine("* Тема:  SprintReview                                                    *");
+            Console.WriteLine("* Задание #TaskReview                                                    *");
+            Console.WriteLine("* Вариант #8                                                             *");
+            Console.WriteLine("* Выполнил: Мартысевич Андрей Денисович | ИИПб-23-3                      *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр '264795863157'.                      *");
+            Console.WriteLine("* Преобразуйте ее в матрицу 3 на 4 и подсчитайте                         *");
+            Console.WriteLine("* сумму нечетных чисел в матрице.                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            int n = 3;
+            int m = 4;
+            int[,] matrix = new int[n, m];
+            string value = "264795863157";
+            int index = 0;
+            DataService ds = new DataService();
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{value[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            int res = ds.Calculate(n, m, value);
+            Console.WriteLine("Сумма нечетных элементов = " + res);
+            Console.ReadKey();
+        }
+    }
+}
